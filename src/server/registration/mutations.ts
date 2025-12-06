f49adecf1f397cccc0c97a9a7e58856288cc772a
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-12-07 01:57:30 +0800

    feat: Registration Submission started

--- src/server/registration/mutations.ts
+++ src/server/registration/mutations.ts
@@ -1,1 +1,13 @@
 "use server";
+
+import { StandardRegistrationSchema } from "@/lib/validation/registration/standard";
+
+export const submitRegistration = async (data: StandardRegistrationSchema) => {
+  const parsedData = StandardRegistrationSchema.parse(data);
+
+  console.log("Full registration data:", parsedData);
+
+  console.log("other registrants:", parsedData.step2.otherRegistrants);
+
+  return;
+};
