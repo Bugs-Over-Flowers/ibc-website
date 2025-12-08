f39b43f6ebf133d6d40710774653416aee4d5105
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-12-09 05:33:05 +0800

    fix: fixed on tryCatch

--- src/lib/server/tryCatch.ts
+++ src/lib/server/tryCatch.ts
@@ -2,22 +2,28 @@ import type { ServerFunction, ServerFunctionResult } from "./types";
 
 function tryCatch<TInput extends unknown[], TOutput>(
   fn: (...args: TInput) => Promise<TOutput>,
-): ServerFunction<TInput, TOutput, Error>;
+): ServerFunction<TInput, TOutput, string>;
 
 function tryCatch<TOutput>(
   promise: Promise<TOutput>,
-): Promise<ServerFunctionResult<TOutput>>;
+): Promise<ServerFunctionResult<TOutput, string>>;
 
-function tryCatch<TInput extends unknown[], TOutput>(
+function tryCatch<TInput extends unknown[], TOutput = string>(
   fnOrPromise: ((...args: TInput) => Promise<TOutput>) | Promise<TOutput>,
-): ServerFunction<TInput, TOutput> | Promise<ServerFunctionResult<TOutput>> {
-  const handleSuccess = (data: TOutput): ServerFunctionResult<TOutput> => ({
+):
+  | ServerFunction<TInput, TOutput, string>
+  | Promise<ServerFunctionResult<TOutput, string>> {
+  const handleSuccess = (
+    data: TOutput,
+  ): ServerFunctionResult<TOutput, string> => ({
     success: true,
     data,
     error: null,
   });
 
-  const handleError = (error: unknown): ServerFunctionResult<TOutput> => {
+  const handleError = (
+    error: unknown,
+  ): ServerFunctionResult<TOutput, string> => {
     const message = error instanceof Error ? error.message : String(error);
     return { success: false, data: null, error: message };
   };
