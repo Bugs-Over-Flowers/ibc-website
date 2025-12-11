d24a8008064a7bc1f3c03ac05dbe4500ec9f113c
xiaokjxiao <krystaljane.siaotong-23@cpu.edu.ph>
2025-12-11 20:30:21 +0800

    refactor: remove unused LogIn import from Header component

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LogIn, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
 import { motion } from "motion/react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
