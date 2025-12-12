2a1437d07af80e6af76eeb03af0d31ae1e461483
xiaokjxiao <krystaljane.siaotong-23@cpu.edu.ph>
2025-12-12 16:40:22 +0800

    feat: add  MembersHero components for displaying hero section

--- src/app/(public)/members/page.tsx
+++ src/app/(public)/members/page.tsx
@@ -1,3 +1,14 @@
+import { Footer } from "@/components/navbar/Footer";
+import { Header } from "@/components/navbar/Header";
+import { MembersHero } from "./_components/MembersHero";
+
 export default function Page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <Header />
+      <MembersHero />
+      <Footer />
+      page
+    </div>
+  );
 }
