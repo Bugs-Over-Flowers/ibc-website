3373446be44e1c5183e50fc625b10dc8b6c33e1f
kailouss <kaizensomosera.04@gmail.com>
2025-12-12 19:25:51 +0800

    refactor: type/event.ts

--- src/app/admin/events/types/event.ts
+++ src/app/admin/events/types/event.ts
@@ -1,21 +1,6 @@
-import { z } from "zod";
+import type { Tables } from "@/lib/supabase/db.types";
 
-export const eventSchema = z.object({
-  eventId: z.string(),
-  eventTitle: z.string(),
-  eventHeaderUrl: z.string().nullable(),
-  description: z.string().nullable(),
-  eventStartDate: z.string().nullable(),
-  eventEndDate: z.string().nullable(),
-  venue: z.string().nullable(),
-  eventType: z.string().nullable(),
-  registrationFee: z.number().nullable(),
-  updatedAt: z.string().nullable(),
-  publishedAt: z.string().nullable(),
-});
-
-export type Event = z.infer<typeof eventSchema>;
-
+export type Event = Tables<"Event">;
 export type EventWithStatus = Event & {
   computedStatus: "draft" | "published" | "finished";
 };
