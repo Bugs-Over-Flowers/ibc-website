c0b98df03557e2fdb767f6f0197b346ffde1c8bd
kailouss <kaizensomosera.04@gmail.com>
2025-12-14 14:12:29 +0800

    feat: skeleton for sidebar and admin event page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AdminSidebar } from "./_components/sidebar/AdminSidebar";
 import { MobileHeader } from "./_components/sidebar/MobileHeader";
+import { AdminSidebarSkeleton } from "./_components/sidebar/Skeleton/AdminSidebarSkeleton";
+import { MobileHeaderSkeleton } from "./_components/sidebar/Skeleton/MobileHeaderSkeleton";
 
 export default function AdminLayout({
   children,
@@ -11,12 +13,14 @@ export default function AdminLayout({
   return (
     <SidebarProvider>
       <div className="flex min-h-screen bg-gray-50">
-        <Suspense>
+        <Suspense fallback={<AdminSidebarSkeleton />}>
           <AdminSidebar />
         </Suspense>
 
         <main className="flex-1 overflow-auto">
-          <MobileHeader />
+          <Suspense fallback={<MobileHeaderSkeleton />}>
+            <MobileHeader />
+          </Suspense>
           <div className="p-4 md:p-6">
             <div className="mb-4 hidden md:block">
               <SidebarTrigger />
