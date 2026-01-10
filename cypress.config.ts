bbb329ff4c21733551e2b8fd98d192ac78646779
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-01-10 20:25:49 +0800

    feat: Add Cypress tests and configuration for component and e2e testing. transferred the files to __tests__ folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(_on, _config) {},
+    specPattern: "__tests__/e2e/**/*.cy.{ts,tsx}",
+    supportFile: "__tests__/support/e2e.ts",
   },
 
   experimentalWebKitSupport: true,
@@ -12,5 +14,11 @@ export default defineConfig({
       framework: "next",
       bundler: "webpack",
     },
+    specPattern: "__tests__/component/cypress/**/*.cy.{ts,tsx}",
+    supportFile: "__tests__/support/component.ts",
   },
+
+  fixturesFolder: "__tests__/__fixtures__/cypress",
+  screenshotsFolder: "__tests__/cypress-screenshots",
+  videosFolder: "__tests__/cypress-videos",
 });
