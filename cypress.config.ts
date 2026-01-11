9891d3712eda56ebb3f1ab670b08d9f7bb855e72
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-01-12 00:01:26 +0800

    feat: added a component-index.html for component tests, added additional configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
+import path from "node:path";
 import { defineConfig } from "cypress";
 import { config as dotenvConfig } from "dotenv";
 
 dotenvConfig({ path: ".env.testing" });
 
 export default defineConfig({
+  projectId: "tge28d",
   e2e: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
     setupNodeEvents(_on, _config) {},
     specPattern: "__tests__/e2e/**/*.cy.{ts,tsx}",
     supportFile: "__tests__/support/e2e.ts",
@@ -16,7 +19,15 @@ export default defineConfig({
     devServer: {
       framework: "next",
       bundler: "webpack",
+      webpackConfig: {
+        resolve: {
+          alias: {
+            "@": path.resolve(__dirname, "./src"),
+          },
+        },
+      },
     },
+    indexHtmlFile: "__tests__/support/component-index.html",
     specPattern: "__tests__/component/cypress/**/*.cy.{ts,tsx}",
     supportFile: "__tests__/support/component.ts",
   },
