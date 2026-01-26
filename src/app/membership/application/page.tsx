20fdd2cee65a75bdc7de13bf4267a3950bab2673
John Rofer Casio <jrofer.casio11@gmail.com>
2026-01-26 22:37:38 +0800

    refactor: made sectors based on the database

--- src/app/membership/application/page.tsx
+++ src/app/membership/application/page.tsx
@@ -1,7 +1,12 @@
 import { cookies } from "next/headers";
+import { cookies } from "next/headers";
 import { Suspense } from "react";
 import { getSectors } from "@/server/membership/queries/getSectors";
-import { MembershipApplicationFormWrapper } from "./_components/MembershipApplicationFormWrapper";
+import { MembershipApplicationForm } from "./_components/MembershipApplicationForm";
+
+export default async function MembershipApplicationPage() {
+  const cookieStore = await cookies();
+  const sectors = await getSectors(cookieStore.getAll());
 
 export default async function MembershipApplicationPage() {
   const cookieStore = await cookies();
@@ -10,7 +15,7 @@ export default async function MembershipApplicationPage() {
   return (
     <div className="container mx-auto px-4 py-8 md:py-25">
       <Suspense>
-        <MembershipApplicationFormWrapper sectors={sectors} />
+        <MembershipApplicationForm sectors={sectors} />
       </Suspense>
     </div>
   );
