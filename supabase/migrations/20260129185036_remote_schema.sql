1c30f4032633939c18e139f492660cd1d834cec3
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-01-30 03:18:36 +0800

    chore: pulled policies in the storages

--- supabase/migrations/20260129185036_remote_schema.sql
+++ supabase/migrations/20260129185036_remote_schema.sql
@@ -1,54 +1,82 @@
-create policy "allow admins to do operations jqdsdq_0"
+
+  create policy "allow admins to do operations jqdsdq_0"
   on "storage"."objects"
   as permissive
   for insert
   to authenticated
 with check ((bucket_id = 'headerimage'::text));
-create policy "allow admins to do operations jqdsdq_1"
+
+
+
+  create policy "allow admins to do operations jqdsdq_1"
   on "storage"."objects"
   as permissive
   for select
   to authenticated
 using ((bucket_id = 'headerimage'::text));
-create policy "allow admins to do operations jqdsdq_2"
+
+
+
+  create policy "allow admins to do operations jqdsdq_2"
   on "storage"."objects"
   as permissive
   for update
   to authenticated
 using ((bucket_id = 'headerimage'::text));
-create policy "allow admins to do operations jqdsdq_3"
+
+
+
+  create policy "allow admins to do operations jqdsdq_3"
   on "storage"."objects"
   as permissive
   for delete
   to authenticated
 using ((bucket_id = 'headerimage'::text));
-create policy "allow all operations for admins 19dgg40_0"
+
+
+
+  create policy "allow all operations for admins 19dgg40_0"
   on "storage"."objects"
   as permissive
   for insert
   to authenticated
 with check ((bucket_id = 'logoimage'::text));
-create policy "allow all operations for admins 19dgg40_1"
+
+
+
+  create policy "allow all operations for admins 19dgg40_1"
   on "storage"."objects"
   as permissive
   for select
   to authenticated
 using ((bucket_id = 'logoimage'::text));
-create policy "allow all operations for admins 19dgg40_2"
+
+
+
+  create policy "allow all operations for admins 19dgg40_2"
   on "storage"."objects"
   as permissive
   for update
   to authenticated
 using ((bucket_id = 'logoimage'::text));
-create policy "allow all operations for admins 19dgg40_3"
+
+
+
+  create policy "allow all operations for admins 19dgg40_3"
   on "storage"."objects"
   as permissive
   for delete
   to authenticated
 using ((bucket_id = 'logoimage'::text));
-create policy "insert for all 19dgg40_0"
+
+
+
+  create policy "insert for all 19dgg40_0"
   on "storage"."objects"
   as permissive
   for insert
   to public
 with check ((bucket_id = 'logoimage'::text));
+
+
+
