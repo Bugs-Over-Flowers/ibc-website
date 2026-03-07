09749c5d77d4966daf4f37270591393ec8171c04
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-03-07 10:13:04 +0800

    fix: typescript compiler not working

--- src/lib/export/excel.ts
+++ src/lib/export/excel.ts
@@ -1,5 +1,5 @@
 import type { ColumnDef } from "@tanstack/react-table";
-import writeXlsxFile from "write-excel-file/browser";
+import writeXlsxFile from "write-excel-file";
 
 export interface ExportToExcelOptions<TData> {
   /**
