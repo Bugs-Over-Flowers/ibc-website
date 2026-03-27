20471b82b34df20d8a91beb3dfbc025aaaaa4467
kailouss <154358616+kailouss@users.noreply.github.com>
2026-03-27 13:10:13 +0800

    fix: biome version and removed imports

--- src/app/admin/application/[id]/_components/ApplicationHeader.tsx
+++ src/app/admin/application/[id]/_components/ApplicationHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AlertTriangle, CircleCheckBig, History, XCircle } from "lucide-react";
+import { History } from "lucide-react";
 import type { Route } from "next";
 import Image from "next/image";
 import Link from "next/link";
