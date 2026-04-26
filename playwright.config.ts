eed44a025346925d9f1b4394bbf9be7cd5d159ec
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-04-27 04:50:58 +0800

    test: added a timeout and a global timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: "html",
+  globalTimeout: 10000,
+  timeout: 30000,
 
   use: {
     baseURL: "http://localhost:3000",
